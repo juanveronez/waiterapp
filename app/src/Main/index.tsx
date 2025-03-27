80389324961375038cf434cbe3fe5c94b79f924d
Juan Veronez <juan.monteirov@gmail.com>
2025-03-27 18:52:37 -0300

    Create table modal

--- app/src/Main/index.tsx
+++ app/src/Main/index.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { Button } from "../components/Button";
 import { Categories } from "../components/Categories";
 import { Header } from "../components/Header";
 import { Menu } from "../components/Menu";
+import { TableModal } from "../components/TableModal";
 import {
   Container,
   CategoriesContainer,
@@ -11,6 +13,9 @@ import {
 } from "./styles";
 
 export function Main() {
+  const [isTableModalVisible, setIsTableModalVisible] = useState(false);
+  const [selectedTable, setSelectedTable] = useState<null | string>(null);
+
   return (
     <>
       <Container>
@@ -26,9 +31,19 @@ export function Main() {
       </Container>
       <Footer>
         <FooterSafeArea>
-          <Button>Novo pedido</Button>
+          {!selectedTable && (
+            <Button onPress={() => setIsTableModalVisible(true)}>
+              Novo pedido
+            </Button>
+          )}
         </FooterSafeArea>
       </Footer>
+
+      <TableModal
+        onSave={setSelectedTable}
+        onClose={() => setIsTableModalVisible(false)}
+        visible={isTableModalVisible}
+      />
     </>
   );
 }
---
+++ app/src/components/TableModal/index.tsx
@@ -0,0 +1,50 @@
+import { Modal, Platform, TouchableOpacity } from "react-native";
+import { Text } from "../Text";
+import { ModalBody, Overlay, Header, Form, Input } from "./styles";
+import { Close } from "../Icons/Close";
+import { Button } from "../Button";
+import { useState } from "react";
+
+interface TableModalProps {
+  visible: boolean;
+  onClose: () => void;
+  onSave: (table: string) => void;
+}
+
+export function TableModal({ visible, onClose, onSave }: TableModalProps) {
+  const [tableInput, setTableInput] = useState("");
+
+  function handleSave() {
+    onSave(tableInput);
+    onClose();
+  }
+
+  return (
+    <Modal animationType="fade" visible={visible} transparent>
+      <Overlay behavior={Platform.OS === "android" ? "height" : "padding"}>
+        <ModalBody>
+          <Header>
+            <Text weight="600">Informe a mesa</Text>
+
+            <TouchableOpacity onPress={onClose}>
+              <Close color="#666" />
+            </TouchableOpacity>
+          </Header>
+
+          <Form>
+            <Input
+              placeholder="Número da mesa"
+              placeholderTextColor="#666"
+              keyboardType="number-pad"
+              onChangeText={setTableInput}
+            />
+          </Form>
+
+          <Button onPress={handleSave} disabled={!tableInput}>
+            Salvar
+          </Button>
+        </ModalBody>
+      </Overlay>
+    </Modal>
+  );
+}
